219ac4c83f74813a4bc73c5ceab6711fee42df09
AJ <ajw53@NJIT.edu>
2021-12-21 13:19:16 -0500

    Feature: Added GUID setup

--- URLShortener.py
+++ URLShortener.py
@@ -14,3 +14,18 @@ if auth_res.status_code == 200:
 else:
     print("[!] ERROR! Cannot access token. Process cancelled. [!]")
     exit()
+
+# GUID Setup
+
+# Construct the request headers with auth
+headers = {"Authorization": f"Bearer {access_token}"}
+
+# Get the GUID associated with my account
+groups_res = requests.get("https://api-ssl.bitly.com/v4/groups", headers=headers)
+if groups_res.status_code == 200:
+    # If response is OK, get GUID
+    groups_data = groups_res.json()['groups'][0]
+    guid = groups_data['guid']
+else:
+    print("[!] ERROR! Cannot get GUID. Process cancelled. [!]")
+    exit()
